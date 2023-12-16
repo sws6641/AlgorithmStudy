492b5000e727f1df76dec1085bbba73ee10c50be
sws6641 <sws6641@naver.com>
2023-12-16 16:57:37 +0900

    feat: PGR_12914

---
+++ AlgorithmStudy/src/Algo/DP/PGR_12914.java
@@ -0,0 +1,22 @@
+package Algo.DP;
+
+public class PGR_12914 {
+    class Solution {
+        public long solution(int n) {
+
+            long dp[] = new long[n+1];
+
+            if(n == 1) return 1;
+
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for(int i = 3 ; i < n+1 ; i++){
+                dp[i] = (dp[i-1]+dp[i-2]) % 1234567;
+            }
+
+            return dp[n]% 1234567;
+
+        }
+    }
+}
